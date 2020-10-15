ed2354727add4721a9d68f2d122dfb1a986e0267
Vaibhav Pathade <vaibhavpathade3@outlook.com>
2020-10-16 01:59:51 +0530

    Created new project

---
+++ SendEmailAutomatic/SendEmailAutomatic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendEmailAutomatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
